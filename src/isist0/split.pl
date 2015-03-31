abbd3fee3bc7f524ffe8e2f1d249ac857b53438d
ogdenpm <mark.pm.ogden@btinternet.com>
2015-03-31 17:19:57 +0100

    removed split.pl as now in general unpack.pl routine

--- src/isist0/split.pl
+++
@@ -1,15 +0,0 @@
-open (SRC, "<isis_t0.plm") || die;
-
-while ($_ = <SRC>) {
-	if (/^/) {
-		chomp;
-		s///;
-		$file = $_;
-		open (DST, ">$file") || die;
-	} else {
-		print DST;
-	}
-}
-
-close DST;
-
