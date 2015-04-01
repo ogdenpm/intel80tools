5b9dca9fe06757e31608c2f3cd745e6a4f6ad01a
ogdenpm <mark.pm.ogden@btinternet.com>
2015-04-01 11:06:22 +0100

    rename of combined source file and removal of old split/save utilities

--- src/asm80/save.pl
+++
@@ -1,15 +0,0 @@
-open (SRC, "<asm80a.plm") || die;
-
-while ($_ = <SRC>) {
-	if (/^/) {
-		chomp;
-		s///;
-		$file = $_;
-		open (DST, ">$file") || die;
-	} else {
-		print DST;
-	}
-}
-
-close DST;
-
