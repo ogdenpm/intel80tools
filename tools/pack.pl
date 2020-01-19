c21b6f0cc38871fb85a1adef3251cfe3b3a76f53
Mark <mark.pm.ogden@btinternet.com>
2020-01-19 17:39:42 +0000

    Modified the pack.pl and unpack.pl utilities to offer more capabilities. This also resulted in a change to how nested packed files are handled. The two usages in the repository have been updated Fixed isdm3.2 makefile to note verify is not implemented Removed spurious tmp file in itools/pasc80_2.0

--- tools/pack.pl
+++ tools/pack.pl
@@ -1,33 +1,176 @@
 use Cwd;
 
-my $dir = getcwd;	# use directory name as prefix for merged file
-my $target = (split '/', $dir)[-1] . "_all.src";
+
+sub usage {
+    print <<"END";
+create a packed text file
+
+usage: pack.pl [-h] [-a pattern | -c pattern | -d pattern | -l | -u] [-f] [file]
+
+where -h            prints simple help and exits
+      -a pattern    add text files matching pattern - also updates changed files
+      -c pattern    create new packed file from text files matching pattern
+      -d pattern    remove text files matching pattern
+      -l            list names of included files
+      -u            update files in existing packed file 
+      -f            files only no directories
+      file          an optional target file - default is {curdir}_all.src
+
+      default operation is -c "*"
+
+      patterns are case insensitive ? matches any char * matches any number of chars
+      multiple patterns are separated by |
+      [..] matches ranges of chars and spaces should not be escaped
+      e.g. to match a file name with a space use "* *"
+
+END
+    exit();
+}
+
+my $dst;
+my $recurse = 1;
+my $pattern = "*";
+my %fileList;
+my %fileStatus;
+my $operation;
+my ($ADD, $CREATE, $DELETE, $UPDATE, $LIST, $COPY) = (1, 2, 3, 4, 5, 6);
+my @status = ("", "Added", "", "Deleted", "Updated", "List", "Copy");
+
+my $target = (split /\/|\\/, getcwd)[-1] . "_all.src";
 $target =~ tr/A-Z/a-z/;
 
-sub addfiles {
-	my $dir = $_[0];
-	my @f = glob("$dir/*");
 
-	foreach  (@f) {
-		tr/A-Z/a-z/;
-		next if $_ eq "./$target";
-		next if $_ eq "./makefile";
-		if (-d) {
-			addfiles($_);
-		} elsif (-f && -T) {
-			$out = $_;
-			$out =~ s/^\.\///;
-			print "add $out\n";
-			print DST "$out\n";
-			open(SRC, "<$_") || die "$_:$!";
-			print DST <SRC>;
-			print DST "\cL---\n" if $out =~ /_all\.src$/; # mark end of embedded packed file
-			close SRC;
-		} else {
-			print "skipping file $_\n";
-		}
-	}
+sub getExisting {
+    if (open my $src, "<", $target) {
+        my @files = split /\cL([^\?]\S*)\n/, do { local $/; <$src> };
+        close $src;
+
+        for (my $i = 1; $i < $#files; $i += 2) { 
+            $files[$i] =~ s/`/ /g;                     # spaced stored as ` in packed file
+            $fileList{$files[$i]} = $files[$i + 1];
+            $fileStatus{$files[$i]} = $COPY;
+        }
+        close $src;
+    }
 }
-open(DST, ">", $target) || die "$target:$!";
-addfiles(".");
-close DST;
+
+sub getFiles {
+    my $ndir = $_[0] || "";
+	opendir my $dir, ($ndir || '.');
+
+    while (my $file = readdir $dir) {
+        next if $file eq '.' || $file eq '..';          # ignore . and ..
+        $file = $ndir eq "" ? $file : "$ndir/$file";
+		if (-d $file) {
+            if ($recurse) {
+                getFiles($file);
+            } else {
+                print "skipping directory $file\n";
+            }
+		} else {
+            next unless $file =~ /^$pattern$/xi;            # match those to include
+            next if lc($file) eq $target;                   # avoid add of target
+            if (-f $file && -T $file) {
+                next if $operation == $UPDATE && !defined($fileStatus{$file});
+                if (open my $src, "<", $file) {
+                    my $content = do { local $/; <$src> };
+                    close $src;
+                    $content =~ s/\cL(\S+)$/\cL?\1/mg;  # nested ^Lfile to ^L?file
+                    if (defined($fileStatus{$file})) {
+                        if ($fileList{$file} ne $content) {
+                            $fileStatus{$file} = $UPDATE;
+                        }
+                    } else {
+                        $fileStatus{$file} = $ADD;
+                    }
+                    $fileList{$file} = $content;
+                } else {
+                    print "warning could not read $file\n";
+                }
+            } else {
+                print "skipping binary file $file\n";
+            }
+        }
+    }
+    closedir $dir;
+}
+
+
+
+
+
+sub deleteFiles {
+    $pattern = "(.*\/)?$pattern" if ($recurse);
+    for $f (keys %fileList) {
+        $fileStatus{$f} = $DELETE if $f =~ /^$pattern$/xi;
+    }
+}
+
+
+# use current directory as a prefix for the generated file
+my $opt;
+my $operation = 0;
+
+while ($opt = shift @ARGV) {
+    last unless $opt =~ /^-/;
+    if ($opt eq "-h") {
+        usage();
+    } elsif ($opt eq "-a") {
+        usage() unless ($pattern = shift @ARGV) && !$operation;
+        $operation = $ADD;
+    } elsif ($opt eq "-c") {
+        usage() unless ($pattern = shift @ARGV) && !$operation;
+        $operation = $CREATE;
+    } elsif ($opt eq "-d") {
+        usage() unless ($pattern = shift @ARGV) && !$operation;
+        $operation = $DELETE;
+    } elsif ($opt eq "-l") {
+        usage() unless !$operation;
+        $operation = $LIST;
+    } elsif ($opt eq "-u") {
+        usage() unless !$operation;
+        $operation = $UPDATE;
+    } elsif ($opt eq "-f") {
+        $recurse = 0;
+    } else {
+        usage();
+    }
+}
+
+$operation = $CREATE unless $operation;
+
+$target = $opt if $opt ne "";
+
+$pattern =~ s/([\{\}\$ \.\@])/\\\1/g;       # escape perl pattern chars [] are allowed
+$pattern =~ s/([\*\?])/\.\1/g;              # convert * and ? to perl .* .?
+
+$pattern = "(.*\/)?$pattern" if $recurse;
+
+getExisting() unless $operation == $CREATE;
+if ($operation != $DELETE) {
+    getFiles() if $operation != $LIST;
+} else {
+    deleteFiles();
+}
+
+
+if ($operation != $LIST) {
+    open($dst, ">", "$target.tmp") || die "$target.tmp:$!";
+    for $f (sort keys %fileList) {
+        if ($fileStatus{$f} != $DELETE) {
+            my $packedName = $f;
+            $packedName =~ s/ /`/g;
+            print $dst "\cL$packedName\n";
+            print $dst $fileList{$f};
+        }
+        print "$status[$fileStatus{$f}] $f\n" unless $fileStatus{$f} == $COPY;
+    }
+    close $dst;
+    unlink $target if -f $target;
+    rename "$target.tmp", $target;
+} else {
+    for $f (sort keys %fileList) {
+        print "$f\n";
+    }
+}
+
--- unpack.pl
+++ unpack.pl
@@ -1,48 +1,79 @@
 use File::Path qw(make_path);
+use Cwd;
 
-$sfile = glob("*_all.src");
+sub usage {
+    print <<"END";
+unpack an editable packed file
 
-{
-	open (SRC, "<$sfile") || die "can't open $sfile\n";
-	local $/;
-	$src = <SRC>;
-	close SRC;
+usage: unpack.pl [-r] [-f] [file]
+
+where   -r      unpacks recursively
+        -f      forces overwrite even if existing file is newer and changed
+        file    packed file - defaults to {curdif}_all.src
+
+The packed file is a concatenation of multiple files each one stored as
+
+    ^Lfilename 
+    contents of file
+
+File names beginning with ? are treated as nested file and spaces are replaced by `
+END
+
 }
 
-@files = split /\cL(.+)\n/, $src;
 
-$i = 1;
+my @packed;
+my $recurse = 0;
+my $force = 0;
+my $sfile;
 
-while ($i < $#files) {
-	$userfile = $files[$i++];
-	die "Error unexpected --- marker\n" if $userfile eq "---";
-	# get the reference file taking into account nested packed files
-	$ref = $files[$i++];
-	if ($userfile =~ /_all\.src$/) {
-		$nested = 1;
-		while ($i < $#files) {
-			print "$files[$i]\n";
-			if ($files[$i] eq "---") {
-				$i += 2;
-				last if --$nested == 0;
-				$ref .= "\cL---\n";
-			} else {
-				$nested++ if ($files[$i] =~ /_all\.src$/); 
-				$ref .= "\cL$files[$i++]\n";
-				$ref .= $files[$i++];
-			}
-		}
-	}
-	if (open(FILE, "<$userfile")) {	# open if it exists
-		local $/;
-		$file = <FILE>;
-		close FILE;
-		next if ($file eq $ref);
-	}
-	make_path($1) if $userfile =~ /(.*(\/|\\))/ && ! -d $1; 	# directory
-	print "saving $userfile\n";
-	open(FILE, ">$userfile") or die "can't create $userfile\n";
-	print FILE $ref;
-	close FILE;
+sub unpackFile {
+    my $sfile = $_[0];
+    open (my $src, "<$sfile") || die "can't open $sfile\n";
+    my @files = split /\cL([^\?]\S*)\n/, do { local $/; <$src> };
+    close $src;
+    my $dir = do { $sfile =~ /(.*(\/|\\))/; $1 };
+
+    for (my $i = 1; $i < $#files; $i += 2) {
+        $files[$i] =~ s/`/ /g;          # spaces stored as `
+        $userfile = "$dir$files[$i]";   # file name
+        $ref = $files[$i + 1];          # and its content
+        push @packed, "$userfile" if $ref =~ s/\cL\?/\cL/mg;     # check for a nested file
+        if (open(my $file, "<$userfile")) {	# open if it exists
+            my $content = do { local $/; <$file> };
+            close $file;
+            next if ($content eq $ref); # skip if no change
+            if (!$force && (stat $sfile)[9] < (stat $userfile)[9]) {
+                print "skipping $userfile use -f option to force extract\n";
+                next;
+            }
+        }
+        make_path($1) if $userfile =~ /(.*(\/|\\))/ && ! -d $1;  # make sure containing directory exists
+
+        print "saving $userfile\n";
+        open(my $file, ">$userfile") or die "can't create $userfile\n";
+        print $file $ref;
+        close $file;
+    }
 }
 
+my $opt;
+while ($opt = shift @ARGV) {
+    last unless $opt =~ /^-/;   
+    if ($opt eq '-r') {
+        $recurse++;
+    } elsif ($opt eq '-f') {
+        $force++;
+    }
+}
+
+
+$sfile = $opt || (split /\/|\\/, getcwd)[-1] . "_all.src";
+
+unpackFile($sfile);
+if ($recurse) {
+    while ($sfile = shift @packed) {
+        print "unpacking $sfile\n";
+        unpackFile($sfile);
+    }
+}
