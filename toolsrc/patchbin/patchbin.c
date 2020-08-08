fb13ba4edc38e43bb597930a4da83ab7f52d9100
Mark <mark.pm.ogden@btinternet.com>
2020-08-08 18:22:33 +0100

    Converted to new version management framework

--- toolsrc/patchbin/patchbin.c
+++ toolsrc/patchbin/patchbin.c
@@ -1,6 +1,12 @@
 #include <stdio.h>
 #include <ctype.h>
 #include <stdlib.h>
+#include <stdbool.h>
+#include <string.h>
+#include <stdarg.h>
+
+void showVersion(FILE *fp, bool full);
+char *invokedBy;
 
 char *gethex(char *s, unsigned *val)
 {
@@ -16,6 +22,23 @@ char *gethex(char *s, unsigned *val)
 	return s;
 }
 
+
+void usage(char *fmt, ...) {
+
+
+	showVersion(stderr, false);
+	if (fmt) {
+		va_list args;
+		va_start(args, fmt);
+		putc('\n', stderr);
+		vfprintf(stderr, fmt, args);
+		va_end(args);
+	}
+	fprintf(stderr, "\nUsage: %s -v | patchfile filetopatch\n", invokedBy);
+
+	exit(1);
+}
+
 int main(int argc, char **argv)
 {
 	FILE *pfp, *fp;
@@ -24,12 +47,15 @@ int main(int argc, char **argv)
 	char *s;
     long fsize;
     int append;
+	invokedBy = argv[0];
 
-
-	if (argc != 3 || (pfp = fopen(argv[1], "rt")) == NULL || (fp = fopen(argv[2], "r+b")) == NULL) {
-		fprintf(stderr, "usage: patchbin patchfile filetopatch\n");
-		exit(1);
+	if (argc == 2 && strcmp(argv[1], "-v") == 0) {
+		showVersion(stdout, true);
+		exit(0);
 	}
+	if (argc != 3 || (pfp = fopen(argv[1], "rt")) == NULL || (fp = fopen(argv[2], "r+b")) == NULL)
+		usage(NULL);
+
     fseek(fp, 0, SEEK_END);
     fsize = ftell(fp);
 
