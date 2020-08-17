0d048063ee34cede10cea428580cf71cefcb7d20
Mark <mark.pm.ogden@btinternet.com>
2020-08-17 12:52:50 +0100

    Initial binobj commit

---
+++ c-ports/binobj/binobj.c
@@ -0,0 +1,181 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdint.h>
+#include <ctype.h>
+#include <stdbool.h>
+
+#define MAXNAME 31
+#define MODHDR  2
+#define MODEND  4
+#define CONTENT 6
+#define MODEOF  14
+
+uint16_t readWord(uint8_t *p) {
+    return p[0] + p[1] * 256;
+}
+
+void writeWord(uint8_t *p, uint16_t val) {
+    *p++ = val % 256;
+    *p = val / 256;
+}
+
+/*                                  */
+/*  module header record definition */
+/*                                  */
+struct {
+    uint8_t type;
+    uint8_t length[2];
+    uint8_t nameLen;
+    uint8_t name[MAXNAME + 3];  // name + trnId + trnVn + crc
+} modhdr = { MODHDR };
+
+/* intermediate storate for length & address */
+struct {
+    uint8_t length[2];
+    uint8_t addr[2];
+} binHeader;
+
+struct {
+    uint8_t type;
+    uint8_t length[2];
+    uint8_t segId;
+    uint8_t addr[2];
+} contentHeader = { CONTENT };
+
+/*                                 */
+/*  module end record definition   */
+/*                                 */
+struct {
+    uint8_t recType;
+    uint8_t length[2];
+    uint8_t type;
+    uint8_t segId;
+    uint8_t addr[2];
+    uint8_t crc;
+} modend = { MODEND, 5, 0, 1, 0 };
+
+/*                                 */
+/*  module end of file record      */
+/*  definition                     */
+/*                                 */
+struct {
+    uint8_t type;
+    uint8_t length[2];
+    uint8_t crc;
+} modeof = { MODEOF, 1, 0, 0xf1 };
+
+/* calc crc */
+uint8_t calcCRC(uint8_t *p, int len) {
+    uint8_t crc = 0;
+    while (len-- > 0)
+        crc += *p++;
+    return crc;
+}
+
+// return the trailing filename part of the passed in path
+const char *getFName(const char *path) {
+    const char *t;
+    while (t = strpbrk(path, ":\\/"))       // allow windows & unix separators - will fail for unix if : in filename!!
+        path = t + 1;
+    return path;
+}
+
+/* *-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*
+   *-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*
+
+      BEGINNING OF MAIN PROGRAM.
+      Note, unlike the original this version will does not
+      limit the output block size
+
+   *-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*
+   *-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-* */
+
+int main(int argc, char **argv) {
+    FILE *fpin, *fpout;
+    uint16_t length, addr;
+
+    if (argc != 3) {
+        fprintf(stderr, "usage: %s binfile objfile\n", argv[0]);
+        exit(1);
+    }
+
+    if ((fpin = fopen(argv[1], "rb")) == NULL) {
+        fprintf(stderr, "can't open %s\n", argv[1]);
+        exit(1);
+    }
+    if ((fpout = fopen(argv[2], "wb")) == NULL) {
+        fprintf(stderr, "can't create %s\n", argv[2]);
+        fclose(fpin);
+        exit(1);
+    }
+
+    /*
+     * output module header record, taking name from input file
+     * this version allows upto 31 characters, original limited at 6
+     * non alpha numeric are deleted and all alpha is converted to uppercase
+     */
+
+    int i = 0;
+    for (const char *name = getFName(argv[1]); *name && *name != '.'; name++)
+        if (isalnum(*name) && i < MAXNAME)
+            modhdr.name[i++] = toupper(*name);
+    modhdr.nameLen = i;
+    writeWord(modhdr.length, i + 4);
+    modhdr.name[i++] = 0;   // TRN ID
+    modhdr.name[i++] = 0;   // TRN VN
+    modhdr.name[i] = -calcCRC((uint8_t *)&modhdr, i + 6);
+    if (fwrite(&modhdr, 1, i + 8, fpout) != i + 8) {
+        fprintf(stderr, "failed to write obj file header\n");
+        exit(1);
+    }
+
+    /*
+     * read all bin records and write as obj records
+     * until length of record is 0
+     */
+    while (true) {
+        if (fread(&binHeader, 1, 4, fpin) != 4) {
+            fprintf(stderr, "unexpected EOF\n");
+            exit(1);
+        }
+        length = readWord(binHeader.length);
+        addr = readWord(binHeader.addr);
+        if (length == 0)
+            break;
+        // now we know length and address of bin block
+        // create the corresponding content header
+        // type & segId initialised already
+        writeWord(contentHeader.length, length + 3);    // data + addr + crc
+        writeWord(contentHeader.addr, addr);
+        if (fwrite(&contentHeader, 1, 6, fpout) != 6) {
+            fprintf(stderr, "failed to write obj file content\n");
+            exit(1);
+        }
+        uint8_t crc = calcCRC((uint8_t *)&contentHeader, 6);
+        while (length-- > 0) {
+            int c;
+            if ((c = getc(fpin)) == EOF) {
+                fprintf(stderr, "unexpected EOF reading bin file\n");
+                exit(1);
+            }
+            crc += c;           // update CRC
+            putc(c, fpout);
+        }
+        putc(-crc, fpout);
+    }
+    /*
+        fill in the missing details of the
+        output the module end record
+    */
+    writeWord(modend.addr, addr);
+    modend.crc = -calcCRC((uint8_t *)&modend, 7);
+    if (fwrite(&modend, 1, 8, fpout) != 8 || fwrite(&modeof, 1, 4, fpout) != 4) {
+        fprintf(stderr, "failed to write obj file end information\n");
+        exit(1);
+    }
+    fclose(fpin);
+    fclose(fpout);
+    return 0;
+}
--- tools/install.cmd
+++ tools/install.cmd
@@ -1,11 +1,11 @@
 @echo off
-setlocal
+setlocal enabledelayedexpansion
 rem locally install applications
-if /I [%~1] == [-v] (echo %0: Rev 6 -- git d716973 [2020-08-12]) & goto :EOF
+if /I [%~1] == [-v] (echo %0: Rev 7 -- git c40c510 [2020-08-13]) & goto :EOF
 if [%1] == [] goto usage
 if [%2] neq [] goto start
 :usage
-echo usage: install file_with_path installRoot [configFile]
+echo usage: %0 file_with_path installRoot [configFile]
 echo        configFile defaults to installRoot\install.cfg
 echo.
 echo install.cfg contains lines of the form type,dir[,suffix]
@@ -22,6 +22,13 @@ echo x86-Release,d:\bin,_32
 echo.
 echo install . path\x86-Release\myfile.exe
 echo copies myfile to .\prebuilt\myfile.exe and d:\bin\myfile_32.exe
+echo.
+echo Control lines are also supported and they change what files the control lines apply to
+echo Each control line's impact continue until the next control line
+echo A control line starting with a + enables processing only for the list of files after the +
+echo One starting  with a - only enables processing for files not in the list
+echo a file name of * matches all files so +* renables processing for all files
+echo -* stops all processing until the next control line (of limited use)
 exit /b 1
 
 :start
@@ -29,7 +36,8 @@ exit /b 1
 :: find the path to the target file
 for %%I in ("%1") do set PATHTO=%%~pI
 :: get the directory as a filename by removing trailing \
-for %%I in ("%PATHTO:~,-1%") do set TYPE=%%~nxI
+for %%I in ("%PATHTO:~,-1%") do set PARENT=%%~nxI
+set FILE=%~nx1
 
 if [%3] == [] (
     set CONFIGFILE=%2\install.cfg
@@ -41,17 +49,43 @@ set CONFIGFILE=%CONFIGFILE:\\=\%
 if not exist %CONFIGFILE% (
     echo cannot find %CONFIGFILE%
     echo.
-    goto :error
+    goto :eof
 )
 :: get the current time for date/time modified dir or suffix
 for /f "tokens=2 delims==." %%A in ('wmic os get LocalDateTime /format:list') do set NOW=%%A
 :: process the intall config file
-for /f "tokens=1,2,* delims=," %%A in (%CONFIGFILE%) do (
-    if /I [%TYPE%] == [%%A] (
-        call :copy "%1" "%2" "%%B" "%%C"
+set INCLUDE=Y
+for /f %%A in (%CONFIGFILE%) do (
+:: cannot set LINE with a comma in the text so convert comma to colon. Need to quote/unquote to do this
+    call :setSkipping ACTION %%A
+    if [!ACTION!] == [COPY] (
+        if [!INCLUDE!] == [Y] (
+            for /f "tokens=1,2,* delims=," %%B in ("%%A") do (
+                if /I [%PARENT%] == [%%B] (
+                    call :copy "%1" "%2" "%%C" "%%D"
+                )
+            )
+        )
+    ) else (
+        call :updateSkipping INCLUDE !ACTION! %FILE% "%%A"
     )
 )
 goto :eof
+
+:setSkipping result line
+setlocal
+set LINE=%~2
+set RESULT=COPY
+if [%LINE%] neq [] (
+    if [%LINE:~,1%] == [-] (
+       set RESULT=EXCLUDE
+    ) else (
+       if [%LINE:~,1%] == [+] set RESULT=INCLUDE
+    )
+)
+endlocal & set %1=%RESULT%
+goto :eof
+
 
 :: the core code to copy the file to the desired location
 :copy root src dir suffix
@@ -80,5 +114,28 @@ echo Installing %~1 to %FILE%
 copy /b /y "%~1" "%FILE%"
 goto :eof
 
-:error
+:updateSkipping var val file match
+setlocal enabledelayedexpansion
+:: because setting a variable with a comma in it isn't supported in windows
+:: modify the input so that that each file is enclosed in []
+set match=%4
+set match=%match:,=][%
+set match=%match:"=%
+set match=[%match:~1%]
+:: now enclose the current file name in []
+set file=[%~3]
+:: try removing from the list (also allow * as a wild card)
+call set removed=!match:%file%=!
+if [%removed%] neq [] set removed=%removed:[*]=%
+if [%removed%] neq [%match%] (
+    if [%2] == [INCLUDE] (set result=Y) else (set result=N)
+) else (
+    if [%2] == [INCLUDE] (set result=N) else (set result=Y)
+)
+
+endlocal & if [%removed%] neq [%match%] set %1=%result%
+goto :eof
+
+
+
 
