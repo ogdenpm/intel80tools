d8ba299ad210c73a0e3d03ff7713338f8239ff9a
Mark Ogden <mark.pm.ogden@btinternet.com>
2025-09-29 14:21:01 +0100

    Modified makefiles to work with busybox ash rather than bash

--- unix/unixtool.cmd
+++ unix/unixtool.cmd
@@ -1,2 +1,2 @@
 REM create a hard link the unix tools based on busybox
-for %%i in (basename cat cmp cp date echo ls mkdir mv rm touch tr) do busybox64u ln -Tf busybox64u.exe %%i.exe 
+for %%i in (ash basename cat cmp cp date echo ls mkdir mv rm touch tr) do busybox64u ln -Tf busybox64u.exe %%i.exe 
